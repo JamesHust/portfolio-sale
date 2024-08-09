935c49e7f5f6c926e1305c40a3595ee206168710
hungmt4 <hungmt4@fpt.com>
2024-08-09 16:53:51 +0700

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,43 +3,50 @@ import BackgroundWrap from "@/components/base/gradient-bg";
 import Image from "next/image";
 import { TextGenerateEffect } from "@/components/base/text-generate-effect";
 import MagicButton from "@/components/base/magic-button";
+import { FloatingNav } from "@/components/base/floating-navbar";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="min-h-screen relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main
+      className="min-h-screen relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <BackgroundWrap />
-      <div className="max-w-7xl w-full">
-        <div className="flex flex-col items-center justify-center relative z-[2]">
-          <Image
-            src="/img/avatar.png"
-            alt="avatar"
-            width={300}
-            height={300}
-            className="rounded-lg w-72"
-          />
-          <div className="flex justify-center">
-            <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-              <TextGenerateEffect
-                words="I'm Nguyen Van A, Sales Manager at FPT Smart Cloud"
-                className="text-center text-2xl md:text-5xl lg:text-6xl"
-              />
+      <div className="max-w-[70%] mx-auto h-full">
+        <FloatingNav navItems={navItems} />
+        <div className="max-w-7xl w-full">
+          <div className="flex flex-col items-center justify-center relative z-[2] lg:mt-4">
+            <Image
+              src="/img/avatar.png"
+              alt="avatar"
+              width={300}
+              height={300}
+              className="rounded-lg w-72"
+            />
+            <div className="flex justify-center">
+              <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+                <TextGenerateEffect
+                  words="I'm Nguyen Van A, Sales Manager at FPT Smart Cloud"
+                  className="text-center text-2xl md:text-5xl lg:text-6xl"
+                />
 
-              <MagicButton
-                title="Show my work"
-                icon={<></>}
-                position="right"
-              />
+                <MagicButton
+                  title="Show my work"
+                  icon={<></>}
+                  position="right"
+                />
+              </div>
             </div>
           </div>
+          {/*<BackgroundWrap />*/}
+          {/*<FloatingNav navItems={navItems} />*/}
+          <WorldWrap
+            className="absolute left-[30%] top-[70%] w-[500px] h-[500px] lg:top-[60%] lg:left-[50%] lg:w-[80%] lg:h-[80%] z-[1]" />
+          {/*<RecentProjects />*/}
+          {/*<Clients />*/}
+          {/*<Experience />*/}
+          {/*<Approach />*/}
+          {/*<Footer />*/}
         </div>
-        {/*<BackgroundWrap />*/}
-        {/*<FloatingNav navItems={navItems} />*/}
-        <WorldWrap className="absolute left-[30%] top-[70%] w-[500px] h-[500px] lg:top-[60%] lg:left-[50%] lg:w-[80%] lg:h-[80%] z-[1]" />
-        {/*<RecentProjects />*/}
-        {/*<Clients />*/}
-        {/*<Experience />*/}
-        {/*<Approach />*/}
-        {/*<Footer />*/}
       </div>
     </main>
   );
--- components/base/gradient-bg/index.tsx
+++ components/base/gradient-bg/index.tsx
@@ -19,12 +19,11 @@ const BackgroundWrap = () => {
       </div>
 
       <div
-        className="min-h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
-       absolute top-0 left-0 flex items-center justify-center"
+        className="min-h-screen w-full bg-black-100 bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center z-0"
       >
         <div
-          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
-         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+          className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
       </div>
     </div>
